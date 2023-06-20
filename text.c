4aa71aba8c35adbbdcfb757ac511f55d5a45aefd
Dao Hau <haudao30042004@gmail.com>
2023-06-20 09:41:47 +0700

    a new project

---
+++ text.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+   int a;
+   
+   printf("nhap vao phep tinh:");
+   scanf("%d", &a);
+   if (a % 2 ==0)
+   {
+    printf("so vua nhap vao chia het cho 2");
+
+   }
+   else
+   {
+   printf("vui long nhap lai");
+   }
+   
+   
+   return 0;
+}
